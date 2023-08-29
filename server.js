3159fb1597d725b4953cc8bd888d9ed2c51718ca
Samuel1878 <samuel1878@users.noreply.github.com>
2023-08-30 01:37:17 +0630

    test_01

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ configRoutes(app);
   db = await connectDB();
 })()
 //start server and listen for connections
-httpServer.listen(process.env.PORT|| 80, (err) => {
+httpServer.listen(process.env.PORT, (err) => {
   if (err) console.log(err);
-  logger.info(`A server is runing at ${process.env.HOST, process.env.PORT}`);
+  logger.info(`A server is runing at ${process.env.PORT}`);
 });
 
 //handle real-time poker game logic with socket.io
